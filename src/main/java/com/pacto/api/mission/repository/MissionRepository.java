3898c54762133d624fea6780484ce1e56dd114de
Jiyeon <jjiyeon27@gmail.com>
2026-05-26 11:47:49 +0900

    feat: 미션 도메인 CRUD 구현

---
+++ src/main/java/com/pacto/api/mission/repository/MissionRepository.java
@@ -0,0 +1,12 @@
+package com.pacto.api.mission.repository;
+
+import com.pacto.api.mission.domain.Mission;
+import com.pacto.api.mission.domain.MissionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
+    List<Mission> findByBloggerIdAndStatus(Long bloggerId, MissionStatus status);
+    List<Mission> findByBloggerId(Long bloggerId);
+}
