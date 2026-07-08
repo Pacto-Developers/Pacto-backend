e2c6c1c06cb01d3b6925f5f47c25003f046e1e01
YSH <yang5864@naver.com>
2026-07-08 20:57:35 +0900

    fix: Swagger 내에서 Request URL HTTPS 서버 URL 수정

--- src/main/java/com/pacto/api/common/SwaggerConfig.java
+++ src/main/java/com/pacto/api/common/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,6 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        // JWT 보안 설정 (프론트엔드가 Swagger에서 토큰을 넣고 테스트할 수 있게 해줌)
         String jwtSchemeName = "jwtAuth";
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
         Components components = new Components()
@@ -23,7 +23,12 @@ public class SwaggerConfig {
                         .scheme("bearer")
                         .bearerFormat("JWT"));
 
+        Server prodServer = new Server()
+                .url("https://pacto-api.duckdns.org")
+                .description("Production server");
+
         return new OpenAPI()
+                .addServersItem(prodServer)
                 .info(new Info()
                         .title("Pacto 프로젝트 API 명세서")
                         .description("B2B2C 에스크로 정산 플랫폼 Pacto의 API 명세입니다.")
